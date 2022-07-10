5cf4010b08bc0bad502c1ae13ae95a26cb9dcecd
wrainf <amirfaiz1806@gmail.com>
2022-07-10 14:48:46 +0800

    completed basic add info functionality

--- src/components/AddInfo.js
+++ src/components/AddInfo.js
@@ -1,9 +1,42 @@
 import React, { Component } from "react";
 
 class AddInfo extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      info: {text: ''},
+      infos: [],
+    }
+
+    this.addInfo = this.addInfo.bind(this);
+  }
+
+  setNewInfo = (e) => {
+    this.setState({
+      info: {
+        text: e.target.value,
+      }
+    })
+  }
+
+  addInfo(){
+    this.setState({
+      infos: this.state.infos.concat(this.state.info.text),
+      info:{
+        text: '',
+      },
+    })
+    console.log(this.state);
+  }
+
+
   render() {
     return (
-      <div>Add Info</div>
+      <div>
+        <div>Add Info</div>
+        <input type="text" onChange={this.setNewInfo}/>
+        <button onClick={this.addInfo}>Add</button>
+      </div>
     )
   }
 }
