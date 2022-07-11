332f3d4f472375b8c546ccfdca4f537348c09097
wrainf <amirfaiz1806@gmail.com>
2022-07-12 01:10:04 +0800

    added basic adding work functionality

--- src/App.js
+++ src/App.js
@@ -1,6 +1,24 @@
+import { Component } from "react";
+import AddInfo from "./components/AddInfo";
+import PersonalInfo from "./components/PersonalInfo"
+import EducationInfo from "./components/EducationInfo"
 
-function App() {
-  return;
+class App extends Component {
+
+
+  render(){
+
+    return(
+      <div id="content">
+        <PersonalInfo/>
+        <EducationInfo/>
+        <div id="add-info">
+          <AddInfo title='Work'/>
+        </div>
+      </div>
+    
+    );
+  }
 }
 
 export default App;
