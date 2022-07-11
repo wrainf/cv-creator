0184988d027ff1cb00d1e4708b9c2d37166799ee
wrainf <amirfaiz1806@gmail.com>
2022-07-12 01:10:47 +0800

    allowed contentediting

--- src/components/PersonalInfo.js
+++ src/components/PersonalInfo.js
@@ -6,14 +6,14 @@ class PersonalInfo extends Component {
     return(
       <div>
         <div>
-          <h1 contentEditable="true" id="name">John Doe</h1>
+          <h1 contentEditable='true' id="name">John Doe</h1>
         </div>
         <div>
-          <p contentEditable="true" id="location">London</p>
+          <p contentEditable='true' id="location">London</p>
           <p>|</p>
-          <p contentEditable="true" id="phone">+44(0)1234567890</p>
+          <p contentEditable='true' id="phone">+44(0)1234567890</p>
           <p>|</p>
-          <p contentEditable="true" id="email">john@gmail.com</p>
+          <p contentEditable='true' id="email">john@gmail.com</p>
         </div>
         
       </div>
