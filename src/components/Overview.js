323b8f4f7b4b11f582c8417a44cd0d639a2c5735
wrainf <amirfaiz1806@gmail.com>
2022-07-12 16:23:36 +0800

    removed ul element

--- src/components/Overview.js
+++ src/components/Overview.js
@@ -7,17 +7,17 @@ const Overview = (props) => {
     <div>
       {infos.map((info) => {
         return (
-          <div>
+          <div id="info">
             <h3>{info.title}</h3>
             <div>
               <h4>{info.role}</h4>
               <h4>{info.duration}</h4>
             </div>
-            <ul>
+            
               <li key={info.id+'1'}>{info.description}</li>
               <li key={info.id+'2'}>{info.achievement}</li>
               <li key={info.id+'3'}>{info.lesson}</li>
-            </ul>
+          
           </div>
         );
       })}
